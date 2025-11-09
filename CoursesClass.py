792a53f3eac8ab03aeee4ef2775ab82fb233a27b
Nawaf <Nwaaf20050@gmail.com>
2025-11-09 19:33:56 +0300

    slightly updated 1.5

--- CoursesClass.py
+++ CoursesClass.py
@@ -4,10 +4,11 @@ import sqlite3
 db_name="RegistrationSystem.db"
 
 class Courses:
-    def __init__(self,Course_code, name, credits, lecture_lab_hours, prerequisites,maximum_capacity):
+    def __init__(self,Course_code, name, credits, lecture_hours,lab_hours, prerequisites,maximum_capacity):
         self.Course_code=Course_code
         self.name=name
         self.credits=credits
-        self.lecture_lab_hours=lecture_lab_hours
+        self.lecture_hours=lecture_hours
+        self.lab_hours=lab_hours
         self.prerequisites=prerequisites
         self.maximum_capacity=maximum_capacity
--- StudentsClass.py
+++ StudentsClass.py
@@ -5,16 +5,13 @@ db_name="RegistrationSystem.db"
 
 class Studnets :
     list_Of_Valid_Programs=["Computer","Power","Biomedical","Communication"]
-    def __init__(self,name,student_id,email,current_level,program=None):
+    def __init__(self,name,student_id,email,program,current_level):
         #Basic student attributes
         self.name=name
         self.student_id=student_id
         self.email=email
         self.current_level=current_level
-        if self.program not in Studnets.list_Of_Valid_Programs:
-            print("Please select a valid program (Computer, Communication, Power, Biomedical).")
-            self.program=program
-        
+        self.program=program
         self.transcript = []                  # List of completed courses (filled later)
 
     def add_to_db(self):
